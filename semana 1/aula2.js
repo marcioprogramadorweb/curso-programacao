e151b78bb3befc2ca33d2ad8c3618967ed8fea83
Marcio Jose dos Santos Almeida <marcioprogramadorweb@gmail.com>
2026-04-24 14:46:04 -0300

    Configura estrutura inicial do curso

---
+++ semana 1/aula2.js
@@ -0,0 +1,12 @@
+let numero1 = 20;
+let numero2 = 4;
+
+let soma = numero1 + numero2;
+let subtracao = numuro1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+console.log("Soma: ", soma);
+console.log("Subtração: ", subtracao);
+console.log("Multiplicação: ", mutiplicacao);
+console.log("Divisão: ", divisao);
---
+++ semana 1/script.js
@@ -0,0 +1,1 @@
+console.log("Javascript criado com sucesso!");
